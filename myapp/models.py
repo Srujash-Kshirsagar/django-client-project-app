7cca8e77a1a6f9265587e3a336c4912419e370fc
Srujash Umesh Kshirsagar <166295389+Srujash-Kshirsagar@users.noreply.github.com>
2024-09-20 00:06:17 +0530

    Second Commit

--- myapp/models.py
+++ myapp/models.py
@@ -1,22 +1,3 @@
-"""
-
-from django.db import models
-from django.contrib.auth.models import User
-
-class Client(models.Model):
-    client_name = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
-    created_by = models.CharField(max_length=255)
-
-class Project(models.Model):
-    project_name = models.CharField(max_length=255)
-    client = models.ForeignKey(Client, on_delete=models.CASCADE)
-    users = models.ManyToManyField(User)
-    created_at = models.DateTimeField(auto_now_add=True)
-    created_by = models.CharField(max_length=255)
-
-"""
-
 from django.db import models
 from django.contrib.auth.models import User
 
